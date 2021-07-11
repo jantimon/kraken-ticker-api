510354152351b922c8b42d10b7711444fdcf5fd7
Jan Nicklas <j.nicklas@me.com>
2021-07-11 11:28:26 +0200

    feat: add generics

--- src/kraken-api-node.ts
+++ src/kraken-api-node.ts
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import { convertToObjectNotation } from "./pairConversion";
+import { PublicTickerResponse } from "./response-types";
 
 /**
  * Get current original kraken asset pairs result
@@ -10,14 +11,16 @@ import { convertToObjectNotation } from "./pairConversion";
  * const bitcoinEth = getAssetPairTickerOriginal('XBTUSD', 'XETHXXBT');
  * ```
  */
-export const getAssetPairTickerOriginal = async (...pairs: string[]) => {
+export const getAssetPairTickerOriginal = async <TKeys extends string>(
+  ...pairs: TKeys[]
+) => {
   const request = await fetch(
     "https://api.kraken.com/0/public/Ticker?pair=" + pairs.join(",")
   );
   const response = await request.text();
   return JSON.parse(response.replace(/\%$/, "")) as {
     error: string[];
-    result: { [key: string]: import("../types").PublicTickerResponse };
+    result: { [key in TKeys]: PublicTickerResponse };
   };
 };
 
@@ -30,8 +33,10 @@ export const getAssetPairTickerOriginal = async (...pairs: string[]) => {
  * const bitcoinEth = getAssetPairTicker('XBTUSD', 'XETHXXBT');
  * ```
  */
-export const getAssetPairTicker = async (...pairs: string[]) => {
-  const response = await module.exports.getAssetPairTickerOriginal(pairs);
+export const getAssetPairTicker = async <TKeys extends string>(
+  ...pairs: TKeys[]
+) => {
+  const response = await getAssetPairTickerOriginal(...pairs);
   return {
     error: response.error,
     result: convertToObjectNotation(response.result),
---
+++ src/response-types.ts
@@ -0,0 +1,41 @@
+export interface PublicTickerResponse {
+  /** ask array(<price>, <whole lot volume>, <lot volume>) */
+  a: [string, string, string];
+  /** bid array(<price>, <whole lot volume>, <lot volume>) */
+  b: [string, string, string];
+  /** last trade closed array(<price>, <lot volume>) */
+  c: [string, string];
+  /** volume array(<today>, <last 24 hours>) */
+  v: [string, string];
+  /** volume weighted average price array(<today>, <last 24 hours>) */
+  p: [string, string];
+  /** number of trades array(<today>, <last 24 hours>) */
+  t: [string, string];
+  /** low array(<today>, <last 24 hours>) */
+  l: [string, string];
+  /** high array(<today>, <last 24 hours>) */
+  h: [string, string];
+  /** today’s opening price */
+  o: string;
+}
+
+export interface ObjectTickerResponse {
+  /** from ticker.a */
+  ask: { price: string; wholeLotVolume: string; lotVolume: string };
+  /** from ticker.b */
+  bid: { price: string; wholeLotVolume: string; lotVolume: string };
+  /** from ticker.c */
+  lastClosedTrade: { price: string; lotVolume: string };
+  /** from ticker.v */
+  volume: { today: string; last24h: string };
+  /** from ticker.p */
+  volumeWeightedAvarage: { today: string; last24h: string };
+  /** from ticker.t */
+  tradeCount: { today: string; last24h: string };
+  /** from ticker.l */
+  low: { today: string; last24h: string };
+  /** from ticker.h */
+  heigh: { today: string; last24h: string };
+  /** from ticker.o */
+  openingPrice: string;
+}
